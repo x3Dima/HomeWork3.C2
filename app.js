5b8d3dc4048a405a5e600307105137afd98aea97
x3Dima <x3dima.romanenko@gmail.com>
2020-10-25 08:27:09 +0200

    new version

--- app.js
+++ app.js
@@ -5,43 +5,43 @@ fetch(URL)
   .then((answer) => answer.json())
   .then((data) => {
     let now = "2020-10-22";
-    let attractionArray = [];
     const usdRates = 28.27;
     const eurRates = 33.53;
     let id = 0;
-
-    for (let item of data) {
+    let bonds = data.filter((item) => {
       let date = item.repaydate.split(".").reverse().join("-");
-      if (date >= now) {
-        let repeatData = {};
-        repeatData.date = date;
-        repeatData.attraction = item.attraction;
-        repeatData.valcode = item.valcode;
-        repeatData.id = id;
-        attractionArray.push(repeatData);
-        id++;
+      if (date > now) {
+        item.date = date;
+
+        return item;
+      }
+    });
+
+    bonds.sort((a, b) => {
+      let dateToNumberA = a.date.split("-").join("");
+      let dateToNumberB = b.date.split("-").join("");
+
+      return +dateToNumberA - +dateToNumberB;
+    });
+
+    for (let item in bonds) {
+      if (bonds[item].valcode == "USD") {
+        bonds[item].attraction * usdRates;
+      } else if (bonds[item].valcode == "EUR") {
+        bonds[item].attraction * eurRates;
       }
     }
-    for (let item in attractionArray) {
-      if (attractionArray[item].valcode == "USD") {
-        console.log(
-          attractionArray[item].id,
-          attractionArray[item].date,
-          attractionArray[item].attraction * usdRates
-        );
-      } else if (attractionArray[item].valcode == "EUR") {
-        console.log(
-          attractionArray[item].id,
-          attractionArray[item].date,
-          attractionArray[item].attraction * eurRates
-        );
-      } else {
-        console.log(
-          attractionArray[item].id,
-          attractionArray[item].date,
-          attractionArray[item].attraction
-        );
+
+    for (let i = 0; i < bonds.length - 1; i++) {
+      if (i === 0) {
+        continue;
+      } else if (bonds[i - 1].date === bonds[i].date) {
+        bonds[i - 1].attraction += bonds[i].attraction;
+        bonds.splice(i - 1, 1);
       }
     }
-    // console.log(attractionArray);
+
+    bonds.forEach((el) => {
+      console.log(el.date, el.attraction);
+    });
   });
